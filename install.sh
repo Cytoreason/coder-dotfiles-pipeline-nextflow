ab2f9173f5cb97bf2838dac68cd966e37fea5085
ag <amihay.gonen@cytroreason.com>
2025-06-11 22:49:34 +0300

    fix

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ if [ ! -d "~/$PROJECT_DIR_NAME" ]; then
     cd
     
     sudo apt-get update && sudo apt-get install tree tldr -y
-    source ~/.bashrc
+
+    # Add NXF_PLUGINS_DIR export to the top of ~/.bashrc if not already present
+grep -qxF 'export NXF_PLUGINS_DIR="/home/coder/pipeline-nextflow/plugins/plugins"' /home/coder/.bashrc || sed -i '1i export NXF_PLUGINS_DIR="/home/coder/pipeline-nextflow/plugins/plugins"' /home/coder/.bashrc
+
     echo "Installing Java"
     curl -s https://get.sdkman.io | bash 
     source "/home/coder/.sdkman/bin/sdkman-init.sh"
@@ -22,7 +25,6 @@ if [ ! -d "~/$PROJECT_DIR_NAME" ]; then
     git clone https://github.com/Cytoreason/$PROJECT_DIR_NAME.git
     cd $PROJECT_DIR_NAME
     git pull 
-
 
     source ~/.bashrc
     echo "Installing VS Code Server Python extensions..."
